babc028c5cbdc6c85467a620ed122c2a207fa611
Vinicius Resende Barbosa <vrb@WINDOWS.CIN.UFPE.BR>
2023-03-01 10:03:57 -0300

    alt create script + fill povoamento

---
+++ criacao_tabelas_alt.sql
@@ -0,0 +1,182 @@
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY';
+
+DROP TABLE Telefones;
+DROP TABLE Funcionario;
+DROP TABLE Vendedor;
+DROP TABLE Cliente;
+DROP TABLE Espaco;
+DROP TABLE Produto;
+DROP TABLE Assistente;
+DROP TABLE Cartao_Fidelidade;
+DROP TABLE Venda;
+DROP TABLE Disponibiliza;
+DROP TABLE Pessoa;
+
+CREATE TABLE Pessoa (
+    cpf_pessoa char(11),
+    nome varchar2(60) not null,
+    email varchar2(60),
+    cep char(8) not null,
+    rua varchar2(60),
+    numero number,
+    complemento varchar2(15),
+    constraint pessoa_pk primary key(cpf_pessoa),
+    -- constraint cpf_pessoa_ck check (cpf_pessoa LIKE '___________'),
+    constraint cpf_pessoa_ck check (REGEXP_LIKE(cpf_pessoa, '\d{11}')),
+    constraint email_ck check (REGEXP_LIKE(email, '(.+)@(.+)\.(.+)')),
+    constraint cep_ck check (cep LIKE '________')
+);
+
+CREATE TABLE Telefones(
+    num_tel varchar2(12),
+    cpf_pessoa char(11),
+    constraint telefones_pk primary key (num_tel, cpf_pessoa),
+    constraint num_tel_ck check (REGEXP_LIKE(num_tel, '(\d{2-3}?)(9?\d{8})')),
+    -- constraint cpf_pessoa_tel_ck check (cpf_pessoa LIKE '___________'),
+    constraint cpf_pessoa_tel_ck check (REGEXP_LIKE(cpf_pessoa, '\d{11}')),
+    constraint cpf_pessoa_tel_fk foreign key (cpf_pessoa)
+        references pessoa(cpf_pessoa)
+);
+
+CREATE TABLE Funcionario (
+    cpf_funcionario char(11),
+    data_adm date not null,
+    funcao varchar2(20) not null,
+    salario number not null,
+    cpf_supervisor char(11) not null unique,
+    constraint salario_ck check (salario >= 0),
+    -- constraint cpf_funcionario_ck check (cpf_funcionario LIKE '___________'),
+    constraint cpf_funcionario_ck check (REGEXP_LIKE(cpf_funcionario, '\d{11}')),
+    -- constraint cpf_supervisor_ck check (cpf_supervisor LIKE '___________'),
+    constraint cpf_supervisor_ck check (REGEXP_LIKE(cpf_supervisor, '\d{11}')),
+    constraint funcionario_pk primary key(cpf_funcionario),
+    constraint supervisor_fk foreign key(cpf_supervisor)
+        references funcionario(cpf_funcionario),
+    constraint cpf_pessoa_func_fk foreign key(cpf_funcionario)
+        references pessoa(cpf_pessoa)
+);
+
+CREATE TABLE Vendedor (
+    cpf_vendedor char(11),
+    data_registro date not null,
+    cnpj char(14),
+    constraint vendedor_pk primary key(cpf_vendedor),
+    -- constraint cpf_vendedor_ck check (cpf_vendedor LIKE '___________'),
+    constraint cpf_vendedor_ck check (REGEXP_LIKE(cpf_vendedor, '\d{11}')),
+    -- constraint cnpj_vend_ck check (cnpj LIKE '______________'),
+    constraint cnpj_vend_ck check (REGEXP_LIKE(cnpj, '\d{14}')),
+    constraint cpf_pessoa_vend_fk foreign key(cpf_vendedor)
+        references pessoa(cpf_pessoa)
+);
+
+CREATE TABLE Cliente (
+    cpf_cliente char(11),
+    cnpj char(14),
+    constraint cliente_pk primary key(cpf_cliente),
+    -- constraint cpf_cliente_ck check (cpf_cliente LIKE '___________'),
+    constraint cpf_cliente_ck check (REGEXP_LIKE(cpf_cliente, '\d{11}')),
+    -- constraint cnpj_cliente_ck check (cnpj LIKE '______________'),
+    constraint cnpj_cliente_ck check (REGEXP_LIKE(cnpj, '\d{14}')),
+    constraint cpf_pessoa_cli_fk foreign key(cpf_cliente)
+        references pessoa(cpf_pessoa)
+);
+
+CREATE TABLE Espaco (
+    cod_espaco number,
+    tamanho char(1) not null,
+    tipo char(1) not null,
+    comissao number not null,
+    cpf_funcionario char(11) not null,
+    constraint espaco_pk primary key(cod_espaco),
+    constraint tamanho_ck check (tamanho in ('P', 'M', 'G')),
+    constraint tipo_ck check (tipo in ('B', 'P')),
+    constraint comissao_ck check (comissao >= 0),
+    -- constraint cpf_funcionario_esp_ck check (cpf_funcionario LIKE '___________'),
+    constraint cpf_funcionario_esp_ck check (REGEXP_LIKE(cpf_funcionario, '\d{11}')),
+    constraint funcionario_esp_fk foreign key(cpf_funcionario)
+        references Funcionario(cpf_funcionario)
+);
+
+CREATE TABLE Produto (
+    cod_produto number,
+    valor number not null,
+    tipo varchar2(25),
+    nome varchar2(50) not null,
+    constraint cod_produto_pk primary key(cod_produto),
+    constraint valor_ck check (valor >= 0)
+);
+
+CREATE TABLE Assistente (
+    cpf_vendedor char(11),
+    nome varchar2(60),
+    constraint assistente_pk primary key (cpf_vendedor, nome),
+    -- constraint cpf_vendedor_ass_ck check (cpf_vendedor LIKE '___________'),
+    constraint cpf_vendedor_ass_ck check (REGEXP_LIKE(cpf_vendedor, '\d{11}')),
+    constraint cpf_vendedor_assist_fk foreign key (cpf_vendedor)
+        references vendedor(cpf_vendedor)
+);
+
+CREATE TABLE Cartao_fidelidade (
+    cpf_cliente char(11),
+    cod_cartao number,
+    constraint cartao_fid_pk primary key (cpf_cliente, cod_cartao),
+    -- constraint cpf_cliente_cart_ck check (cpf_cliente LIKE '___________'),
+    constraint cpf_cliente_cart_ck check (REGEXP_LIKE(cpf_cliente, '\d{11}')),
+    constraint cpf_cliente_cart_fk foreign key (cpf_cliente)
+        references cliente(cpf_cliente)
+);
+
+CREATE TABLE Venda (
+    data_hora timestamp,
+    cod_produto number,
+    cpf_cliente char(11),
+    cpf_vendedor char(11),
+    cpf_funcionario char(11),
+    desconto number,
+    quantidade number not null,
+    valor_unit number not null,
+    valor_total number,
+    constraint venda_pk primary key (data_hora, cod_produto, cpf_cliente, cpf_vendedor),
+    constraint desconto_ck check (desconto BETWEEN 0 and 1),
+    constraint quantidade_ck check (quantidade > 0),
+    constraint valor_unit_ck check (valor_unit >= 0),
+    constraint valor_total_ck check (valor_total = (quantidade * valor_unit) * (1 - desconto) ),
+    -- constraint cpf_cliente_venda_ck check (cpf_cliente LIKE '___________'),
+    constraint cpf_cliente_venda_ck check (REGEXP_LIKE(cpf_cliente, '\d{11}')),
+    -- constraint cpf_func_venda_ck check (cpf_funcionario LIKE '___________'),
+    constraint cpf_func_venda_ck check (REGEXP_LIKE(cpf_funcionario, '\d{11}')),
+    -- constraint cpf_vendedor_venda_ck check (cpf_vendedor LIKE '___________'),
+    constraint cpf_vendedor_venda_ck check (REGEXP_LIKE(cpf_vendedor, '\d{11}')),
+    constraint cod_produto_venda_fk foreign key (cod_produto)
+        references produto(cod_produto),
+    constraint cpf_cliente_venda_fk foreign key (cpf_cliente)
+        references cliente(cpf_cliente),
+    constraint cpf_vendedor_venda_fk foreign key (cpf_vendedor)
+        references vendedor(cpf_vendedor),
+    constraint cpf_funcionario_venda_fk foreign key (cpf_funcionario)
+        references funcionario(cpf_funcionario)
+);
+
+CREATE TABLE Disponibiliza (
+    cpf_vendedor char(11),
+    cpf_funcionario char(11),
+    cod_espaco number,
+    hora_inicio number not null,
+    hora_fim number not null,
+    data_inicio date not null,
+    data_fim date not null,
+    constraint disponibiliza_pk primary key (cpf_vendedor, cpf_funcionario, cod_espaco),
+    constraint hora_inicio_ck check (hora_inicio BETWEEN 0 and 23),
+    constraint hora_fim_ck check (hora_fim BETWEEN 0 and 23),
+    constraint data_fim_ck check (data_fim >= data_inicio),
+    -- constraint cpf_vendedor_disp_ck check (cpf_vendedor LIKE '___________'),
+    constraint cpf_vendedor_disp_ck check (REGEXP_LIKE(cpf_vendedor, '\d{11}')),
+    -- constraint cpf_func_disp_ck check (cpf_funcionario LIKE '___________'),
+    constraint cpf_func_disp_ck check (REGEXP_LIKE(cpf_funcionario, '\d{11}')),
+    constraint cpf_vendedor_disp_fk foreign key (cpf_vendedor)
+        references vendedor(cpf_vendedor),
+    constraint cpf_funcionario_disp_fk foreign key (cpf_funcionario)
+        references funcionario(cpf_funcionario),
+    constraint cod_espaco_disp_fk foreign key (cod_espaco)
+        references espaco(cod_espaco)
+);
---
+++ povoamento_tabelas.sql
@@ -0,0 +1,139 @@
+DROP SEQUENCE cod_prod;
+DROP SEQUENCE cod_esp;
+DROP SEQUENCE cod_fid;
+
+-- criação de sequências
+CREATE SEQUENCE cod_prod
+    MINVALUE 0
+    START WITH 0
+    INCREMENT BY 1;
+    
+CREATE SEQUENCE cod_esp
+    MINVALUE 0
+    START WITH 0
+    INCREMENT BY 1;
+    
+CREATE SEQUENCE cod_fid
+    MINVALUE 0
+    START WITH 0
+    INCREMENT BY 1;
+    
+-- inserção na tabela pessoa
+
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    -- VALUES ('123.456.789-00', 'Vinicius', '123abc@gmail.com', 34550765, 'Rua José de Souza Rodovalho', 45, 'Casa');
+    VALUES ('58235053454', 'Amanda Caldeira', 'ac123@gmail.com', '49038420', 'Rua Orlando Sobral dos Santos', 45, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('44882211491', 'Miguel Alves', 'ma356@outlook.com', '29090820', 'Rua Victorino Cardoso', 2, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('17267832946', 'Ana Vitória Vieira', 'avc259@gmail.com', '78120786', 'Travessa do Nenê', 156);
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('72610547200', 'Pedro Lucas Barbosa', 'plb2526@yahoo.com.br', '49087423', 'Rua B', 43, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, complemento)
+    VALUES ('64784744290', 'João Nascimento', 'jn2@gmail.com', '21511110', 'Rua Gentil de Almeida', 'apt. 21');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('02702912532', 'Calebe Ferreira', 'cf9485@hotmail.com', '88702446', 'Rua Caminho da Luz', 167);
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('21542193818', 'Eduardo Cardoso', 'ec3333@gmail.com', '94828106', 'Rua C', 5, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('85449302711', 'Marcelo Rocha', 'mrq45@gmail.com', '26062420', 'Rua Américo Vespúcio', 48, 'apt. 05');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('26348686850', 'Kamilly Aragão', 'kafd56367@outlook.com', '26060370', 'Rua Dona Eulália', 145, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('37521229517', 'Luana Melo', 'lm@cin.ufpe.br', '84036145', 'Rua Joaquim Jurandir Collares', 202);
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('23238276344', 'Ana Júlia das Neves', 'ajdn0935@gmail.com', '58058340', 'Rua Irene Alves P. Celani', 32, 'apt. 103');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('48174058303', 'Antônio Cardoso', 'ac53h@gmail.com', '49009073', 'Rua José Domingos Maia III', 21, 'apt. 304');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('11467512060', 'Larissa Moura', 'lm@yahoo.com.br', '64088510', 'Rua Pindaíba', 78);
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('31109286635', 'Renan Silveira', 'rsj32j@gmail.com', '64001010', 'Avenida Maranhão', 65, 'apt. 708');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('24366201640', 'Kevin Almeida', 'ka4325@gmail.com', '78132808', 'Avenida Leoncio Lopes de Miranda', 978, 'apt. 501');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('25666674978', 'Carolina Duarte', 'cd87875672678@gmail.com', '78725030', 'Rua General de Marcos', 5, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('75708608706', 'Pedro Lucas Martins', 'plm24243f@gmail.com', '79100290', 'Rua da Promissão', 465, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('36481215706', 'Letícia da Cunha', 'ldc435@gmail.com', '64016745', 'Rua Doutor Area Leão', 307, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('38540174804', 'Bruna Monteiro', 'bmdsaf24@outlook.com', '54080420', 'Rua Marechal Deodoro da Fonsêca', 30, 'apt. 101');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('29586233197', 'Manuela Caldeira', 'mc432659@outlook.com', '76801198', 'Rua Hebert de Azevedo', 20, 'apt. 105');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('41582782792', 'Alice Jesus', 'aj2463@gmail.com', '65630970', 'Rua Magalhães de Almeida', 98);
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('25455186892', 'Luiz Felipe da Costa', 'lfdc@hotmail.com', '78552105', 'Rua das Canelas', 60, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua)
+    VALUES ('89987766420', 'Helena Barbosa', 'hb245@gmail.com', '79114200', 'Rua Oswaldo Goeld');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('18041727662', 'Thales Nogueira', 'tn90825@gmail.com', '76913744', 'Rua Santa Clara', 73, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('70775502227', 'Carolina das Neves', 'cdn09872435@gmail.com', '69039080', 'Rua Herdeiro do Rei', 75, 'apt. 104');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('07262636308', 'Vitor Hugo Caldeira', 'vhc278679375@gmail.com', '84051010', 'Rua Braulina Carneiro de Quadros', 78, 'apt. 606');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, complemento)
+    VALUES ('86385882594', 'Vicente Rocha', 'vr5555@yahoo.com.br', '69313405', 'Rua Raimunda Mendes de Almeida', 'apt. 102');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero, complemento)
+    VALUES ('62704882819', 'Noah Alves', 'nha256@gmail.com', '68906091', 'Rua Santa Tereza', 830, 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, complemento)
+    VALUES ('06661573325', 'Arthur Nogueira', 'athn776543999@gmail.com', '27213000', 'Rua A', 'Casa');
+INSERT INTO pessoa(cpf_pessoa, nome, email, cep, rua, numero)
+    VALUES ('25952848788', 'Isabel Rodrigues', 'ir4432211@gmail.com', '69312593', 'Rua Sobral', 32);
+
+-- inserção na tabela telefones  
+
+INSERT INTO telefones(num_tel, cpf_pessoa)
+    -- VALUES (955684765, '123.456.789-00');
+    VALUES ('36386225', '58235053454');
+INSERT INTO telefones(num_tel, cpf_pessoa)
+    -- VALUES (954384700, '123.456.789-00');
+    VALUES ('7923149467', '58235053454');
+    
+-- inserção na tabela funcionario
+
+INSERT INTO funcionario(cpf_funcionario, data_adm, funcao, salario, cpf_supervisor)
+    VALUES ();
+    
+-- inserção na tabela vendedor
+
+INSERT INTO vendedor(cpf_vendedor, data_registro, cnpj)
+    VALUES ();
+    
+-- inserção na tabela cliente
+
+INSERT INTO cliente(cpf_cliente, cnpj)
+    VALUES ();
+    
+-- inserção na tabela espaco
+
+INSERT INTO espaco(cod_espaco, tamanho, tipo, comissao, cpf_funcionario)
+    VALUES ();
+    
+-- inserção na tabela produto
+
+INSERT INTO produto(cod_produto, valor, tipo, nome)
+    VALUES ();
+    
+-- inserção na tabela assistente
+
+INSERT INTO assistente(cpf_vendedor, nome)
+    VALUES ();
+    
+-- inserção na tabela cartao_fidelidade
+
+INSERT INTO cartao_fidelidade(cpf_cliente, cod_cartao)
+    VALUES ();
+    
+-- inserção na tabela venda
+
+INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
+    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
+    VALUES ();
+    
+-- inserção na tabela disponibiliza
+
+INSERT INTO disponibiliza(cpf_vendedor, cpf_funcionario, cod_espaco,
+    hora_inicio, hora_fim, data_inicio, data_fim)
+    VALUES ();
