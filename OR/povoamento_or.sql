ce44966d6ee90c059fd63f4c002ba30f029e4e23
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-04-03 19:39:31 -0300

    povoamento da tabela Venda

--- OR/povoamento_or.sql
+++ OR/povoamento_or.sql
@@ -986,60 +986,132 @@ INSERT INTO tb_Produto
     
 -- inserção na tabela venda
 
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor, 
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('25/01/2023 14:30:00', 4, '44882211491', '72610547200', '18041727662', 0.07, 7, 4.5, 4.5 * 7 * 0.93);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('27/01/2023 10:15:00', 8, '64784744290', '21542193818', '62704882819', 0.05, 13, 2, 2 * 13 * 0.95);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('28/01/2023 09:00:00', 10, '85449302711', '26348686850', '18041727662', 0, 14, 48, 48 * 14 * 1);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('03/01/2023 09:00:00', 7, '26348686850', '37521229517', '56991573326', 0.02, 11, 1.25, 1.25 * 11 * 0.98);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('03/03/2023 09:00:00', 6, '11467512060', '11467512060', '56991573326', 0, 14, 3.5, 3.5 * 14 * 1);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('03/04/2023 09:00:00', 7, '31109286635', '24366201640', '56991573326', 0, 19, 1.25, 1.25 * 19 * 1);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('05/03/2023 09:00:00', 8, '24366201640', '25666674978', '90805502227', 0.10, 20, 2, 2 * 20 * 0.9);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('02/03/2023 09:00:00', 2, '25666674978', '38540174804', '99124502227', 0.04, 14, 3, 3 * 14 * 0.96);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('03/03/2023 09:00:00', 2, '75708608706', '41582782792', '41582782792', 0.08, 15, 3, 3 * 15 * 0.92);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor, 
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('04/03/2023 09:00:00', 6, '38540174804', '25455186892', '41582782792', 0.09, 7, 3.5, 3.5 * 7 * 0.91);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('05/03/2023 09:00:00', 8, '29586233197', '18041727662', '41582782792', 0.06, 19, 2, 19 * 2 * 0.94);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('06/03/2023 09:00:00', 2, '41582782792', '86385882594', '41582782792', 0.07, 9, 3, 3 * 9 * 0.93);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('02/03/2023 12:23:35', 5, '25666674978', '72610547200', '06402073325', 0.05, 10, 1.8, 1.8 * 10 * 0.95);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('08/03/2023 10:15:00', 3, '85449302711', '21542193818', '18041727662', 0.06, 8, 1.5, 1.5 * 8 * 0.94);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('09/03/2023 09:00:00', 5, '44882211491', '26348686850', '56991573326', 0, 20, 2.2, 2.2 * 20 * 1);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('11/03/2023 09:00:00', 2, '26348686850', '37521229517', '99124502227', 0.03, 10, 4, 4 * 10 * 0.97);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('12/03/2023 09:00:00', 6, '31109286635', '25666674978', '56991573326', 0, 10, 3.5, 3.5 * 10 * 1);
-INSERT INTO venda(data_hora, cod_produto, cpf_cliente, cpf_vendedor,
-    cpf_funcionario, desconto, quantidade, valor_unit, valor_total)
-    VALUES ('13/03/2023 09:00:00', 4, '11467512060', '24366201640', '90805502227', 0.10, 15, 3, 3 * 15 * 0.9);
+INSERT INTO tb_Venda
+    VALUES ('25/01/2023 14:30:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 4),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '44882211491'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '72610547200'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '18041727662'),
+        0.07, 7, 4.5, 4.5 * 7 * 0.93);
+INSERT INTO tb_Venda
+    VALUES ('27/01/2023 10:15:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 8),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '64784744290'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '21542193818'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '62704882819'),
+        0.05, 13, 2, 2 * 13 * 0.95);
+INSERT INTO tb_Venda
+    VALUES ('28/01/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 10),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '85449302711'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '26348686850'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '18041727662'),
+        0, 14, 48, 48 * 14 * 1);
+INSERT INTO tb_Venda
+    VALUES ('03/01/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 7),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '26348686850'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '37521229517'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '56991573326'),
+        0.02, 11, 1.25, 1.25 * 11 * 0.98);
+INSERT INTO tb_Venda
+    VALUES ('03/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 6),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '11467512060'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '11467512060'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '56991573326'),
+        0, 14, 3.5, 3.5 * 14 * 1);
+INSERT INTO tb_Venda
+    VALUES ('03/04/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 7),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '31109286635'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '24366201640'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '56991573326'),
+        0, 19, 1.25, 1.25 * 19 * 1);
+INSERT INTO tb_Venda
+    VALUES ('05/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 8),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '24366201640'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '25666674978'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '90805502227'),
+        0.10, 20, 2, 2 * 20 * 0.9);
+INSERT INTO tb_Venda
+    VALUES ('02/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 2),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '25666674978'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '38540174804'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '99124502227'),
+        0.04, 14, 3, 3 * 14 * 0.96);
+INSERT INTO tb_Venda
+    VALUES ('03/03/2023 15:40:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 2),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '75708608706'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '41582782792'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '41582782792'),
+        0.08, 15, 3, 3 * 15 * 0.92);
+INSERT INTO tb_Venda
+    VALUES ('04/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 6),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '38540174804'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '25455186892'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '41582782792'),
+        0.09, 7, 3.5, 3.5 * 7 * 0.91);
+INSERT INTO tb_Venda
+    VALUES ('05/03/2023 09:27:36',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 8),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '29586233197'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '18041727662'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '41582782792'),
+        0.06, 19, 2, 19 * 2 * 0.94);
+INSERT INTO tb_Venda
+    VALUES ('06/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 2),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '41582782792'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '86385882594'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '41582782792'),
+        0.07, 9, 3, 3 * 9 * 0.93);
+INSERT INTO tb_Venda
+    VALUES ('02/03/2023 12:23:35',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 5),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '25666674978'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '72610547200'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '06402073325'),
+        0.05, 10, 1.8, 1.8 * 10 * 0.95);
+INSERT INTO tb_Venda
+    VALUES ('08/03/2023 10:15:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 3),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '85449302711'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '21542193818'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '18041727662'),
+        0.06, 8, 1.5, 1.5 * 8 * 0.94);
+INSERT INTO tb_Venda
+    VALUES ('09/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 5),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '44882211491'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '26348686850'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '56991573326'),
+        0, 20, 2.2, 2.2 * 20 * 1);
+INSERT INTO tb_Venda
+    VALUES ('11/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 2),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '26348686850'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '37521229517'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '99124502227'),
+        0.03, 10, 4, 4 * 10 * 0.97);
+INSERT INTO tb_Venda
+    VALUES ('12/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 6),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '31109286635'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '25666674978'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '56991573326'),
+        0, 10, 3.5, 3.5 * 10 * 1);
+INSERT INTO tb_Venda
+    VALUES ('13/03/2023 09:00:00',
+        (SELECT REF(p) FROM tb_Produto p WHERE cod_produto = 4),
+        (SELECT REF(c) FROM tb_Cliente c WHERE cpf = '11467512060'),
+        (SELECT REF(v) FROM tb_Vendedor v WHERE cpf = '24366201640'),
+        (SELECT REF(f) FROM tb_Funcionario f WHERE cpf = '90805502227'),
+        0.10, 15, 3, 3 * 15 * 0.9);
 
 -- inserção na tabela disponibiliza
 
